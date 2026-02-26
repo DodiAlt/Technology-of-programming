f35b4f8d9fccd8a221ae59ed6a1a2e536072d953
DodiAlt <164128650+DodiAlt@users.noreply.github.com>
2026-02-26 11:41:40 +0700

    Add files via upload

--- 1 лаба/1 лаба/Program.cs
+++ 1 лаба/1 лаба/Program.cs
@@ -5,25 +5,24 @@ namespace curseValuet
 {
     class Program
     {
-        class Money
-        {
-            public string moneyName1 = "Undef";
-            public string moneyName2 = "Undef";
-            public double curs;
-            public DateOnly date;
-        }
-        public static void Main()
-        {
-            Money money = new Money();
-            string input = Console.ReadLine();
-            string[] partsfild = input.Split(' ');
-            money.moneyName1 = partsfild[0];
-            money.moneyName2 = partsfild[1];
-            money.curs = double.Parse(partsfild[2]);
-            money.date = DateOnly.Parse(partsfild[3]);
-            Console.WriteLine($"Валюта 1: {money.moneyName1}, Валюта 2: {money.moneyName2}, курс первой валюты ко второй: {money.curs} на момент {money.date}");
+        //class Money
+        //{
+        //    public string m_moneyName1 = "Undef";
+        //    public string m_moneyName2 = "Undef";
+        //    public double m_curs;
+        //    public DateOnly m_date;
+        //}
+        //public static void Main()
+        //{
+        //    Money money = new Money();
+        //    string input = Console.ReadLine();
+        //    string[] partsfield = input.Split(' ');
+        //    money.m_moneyName1 = partsfield[0];
+        //    money.m_moneyName2 = partsfield[1];
+        //    money.m_curs = double.Parse(partsfield[2]);
+        //    money.m_date = DateOnly.Parse(partsfield[3]);
+        //    Console.WriteLine($"Валюта 1: {money.m_moneyName1}, Валюта 2: {money.m_moneyName2}, курс первой валюты ко второй: {money.m_curs} на момент {money.m_date}");
 
-        
-        }
+        //}
     }
 }
---
+++ 1 лаба/1 лаба/StringToNumber.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_лаба
+{
+    internal class StringToNumber
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            ExtractPairs(input);
+        }
+
+        static void ExtractPairs(string input)
+        {
+            string[] parts = input.Split('*');
+
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                string currentPart = parts[i];
+                string nextPart = parts[i + 1];
+
+                int num1 = ExtractLastNumber(currentPart);
+                int num2 = ExtractFirstNumber(nextPart);
+
+                if (num1 != -1 && num2 != -1)
+                {
+                    Console.WriteLine($"Найдена пара: {num1} * {num2}");
+                    i++;
+                }
+            }
+
+        }
+
+        static int ExtractLastNumber(string text)
+        {
+            string number = "";
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    number = text[i] + number;
+                }
+                else if (number.Length > 0)
+                {
+                    break;
+                }
+            }
+
+            if (number.Length > 0)
+            {
+                return int.Parse(number);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int ExtractFirstNumber(string text)
+        {
+            string number = "";
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    number += c;
+                }
+                else if (number.Length > 0)
+                {
+                    break;
+                }
+            }
+            if (number.Length > 0)
+            {
+                return int.Parse(number);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
