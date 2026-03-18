1ce735c91db17f32501cd83c1b554a60f0a99f7c
DodiAlt <il.semenckow@yandex.ru>
2026-03-18 13:46:08 +0700

    1 лабораторная

---
+++ Технологии программирования/Технологии программирования/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace curseValuet
+{
+    class Program
+    {
+        class Money
+        {
+            public string m_moneyName1 = "Undef";
+            public string m_moneyName2 = "Undef";
+            public double m_curs;
+            public DateTime m_date;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Введите данные: name name cost date");
+            Money money = new Money();
+            string input = Console.ReadLine();
+            string[] partsfield = input.Split(' ');
+            money.m_moneyName1 = partsfield[0];
+            money.m_moneyName2 = partsfield[1];
+            money.m_curs = double.Parse(partsfield[2]);
+            money.m_date = DateTime.Parse(partsfield[3]);
+            Console.WriteLine($"Валюта 1: {money.m_moneyName1}, Валюта 2: {money.m_moneyName2}, курс первой валюты ко второй: {money.m_curs} на момент {money.m_date}");
+
+        }
+    }
+}
